e6bfd6dffc4a2f395354df41ecf95d8bee701242
Wang Zhuochun <stone1551@gmail.com>
2012-06-09 21:55:58 +0800

    add tooltips and several bootstrap themes
    add tooltips and several bootstrap themes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-/* Author:
+/*
+ * Author: Wang Zhuochun
+ * Last Edit: 09/Jun/2012 06:21 PM
+ */
 
-*/
+(function($) {
+    // enable tooltips
+    $("header li.visible-desktop").tooltip({placement:'bottom', selector:"a[rel=tooltip]"});
+    $("#tab-lists, #pane-contents").tooltip({selector:"a[rel=tooltip]"})
 
-
-
-
-
+})(jQuery);
