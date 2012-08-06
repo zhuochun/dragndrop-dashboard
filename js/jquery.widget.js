4ca45a8b8c578b50183b4941c03b3f5d231f4aea
Wang Zhuochun <stone1551@gmail.com>
2012-08-06 17:54:39 +0800

    add widget setting configs
    - add widget setting configs
    - fix bugs in jquery.widget.js
    - add widget background color change

--- js/jquery.widget.js
+++ js/jquery.widget.js
@@ -47,6 +47,8 @@
         // $elem DOM element
         $this = this.$elem = $(this.elem);
 
+        // this widget's id
+        this.id = $this.attr("id");
         // content DOM element
         this.$content = $this.find(".widget-content");
         // panels DOM element
@@ -54,64 +56,123 @@
         // controls DOM element
         this.$control = $this.find(".widget-control");
 
-        // bind events
-        this.bindEvents();
+        // bind title button events
+        this.bindTitleEvents();
+        // bind setting events
+        if (this.options.controls.setting) {
+            this.bindPanelEvents();
+        }
     };
 
-    Plugin.fn.bindEvents = function() {
+    Plugin.fn.bindPanelEvents = function() {
+        this.BgColorChangeEvent();
+        this.formSubmitEvent();
+    };
+
+    Plugin.fn.BgColorChangeEvent = function() {
+        var
+          that = this
+        , $colorSetting = this.$elem.find("#" + this.id + "-colors")
+        // li for colors
+        , $colors = $colorSetting.find("li");
+
+        // set initial background color again
+        this.background = $colors.filter(".selected").data("color");
+
+        // background color click
+        $colorSetting.on("click", "li", function() {
+            var $this = $(this), newBgColor;
+
+            // update selected color
+            $colors.removeClass("selected");
+            $this.addClass("selected");
+
+            // update widget background color
+            newBgColor = $this.data("color");
+            that.$elem.removeClass(that.background).addClass(newBgColor);
+
+            // save the new background
+            that.background = newBgColor;
+        });
+    };
+
+    Plugin.fn.formSubmitEvent = function() {
+        var
+          that = this
+        , $submit = this.$elem.find("#" + this.id + "-submit")
+        , $cancel = this.$elem.find("#" + this.id + "-cancel");
+
+        $submit.on("click", function(e) {
+            e.preventDefault();
+
+            // TODO submit form to server
+            console.log("form submitted");
+
+            // flip to default panel
+            that.flipPanel();
+
+            return;
+        });
+
+        $cancel.on("click", function(e) {
+            e.preventDefault();
+            // flip to default panel
+            that.flipPanel();
+        })
+    };
+
+    Plugin.fn.bindTitleEvents = function() {
         // bind setting event
         if (this.options.controls.setting) {
             this.currentPanel = this.options.initialized ? 0 : 1;
             // bind Event
-            this.$control.find(".widget-setting").on("click", $.proxy(this.settingEvent, this));
+            this.$control.find(".widget-setting").on("click", $.proxy(this.flipPanel, this));
         }
 
         // bind refresh event
         if (this.options.controls.refresh) {
-            this.$control.find(".widget-refresh").on("click", $.proxy(this.refreshEvent, this));
+            this.$control.find(".widget-refresh").on("click", $.proxy(this.refresh, this));
         }
 
         // bind fold event
         if (this.options.controls.fold) {
-            this.$control.find(".widget-fold").on("click", $.proxy(this.foldEvent, this));
+            this.$control.find(".widget-fold").on("click", $.proxy(this.foldPanel, this));
         }
 
         // bind remove event
         if (this.options.controls.remove) {
-            this.$control.find(".widget-remove").on("click", $.proxy(this.hideEvent, this));
+            this.$control.find(".widget-remove").on("click", $.proxy(this.hideWidget, this));
         }
     };
 
-    Plugin.fn.settingEvent = function() {
-        var
-          active = ".widget-panel-active"
-        , panelDefault = this.$panels.get(this.currentPanel)
-        , panelSetting = this.$panels.get(1 - this.currentPanel);
+    Plugin.fn.flipPanel = function() {
+        var curPanel = this.$panels.get(this.currentPanel)
+          , nxtPanel = this.$panels.get(1 - this.currentPanel);
 
         if (this.$content.is(":hidden")) {
-            if (this.currentPanel !== 1) {
-                $(panelDefault).removeClass(active).hide();
-                $(panelSetting).addClass(active).show();
+            if (this.currentPanel !== 1) { // not setting
+                $(curPanel).hide();
+                $(nxtPanel).show();
                 // update the new current
                 this.currentPanel = 1;
             }
             // slide down
-            this.foldEvent();
+            this.foldPanel();
         } else {
             // hide current
-            $(panelDefault).removeClass(active).slideToggle();
+            $(curPanel).slideToggle();
             // show the other
-            $(panelSetting).addClass(active).slideToggle();
+            $(nxtPanel).slideToggle();
             // update the new current
             this.currentPanel = 1 - this.currentPanel;
         }
     };
 
-    Plugin.fn.refreshEvent = function() {
+    Plugin.fn.refresh = function() {
         window.console.log("Refreshing");
     };
 
-    Plugin.fn.foldEvent = function() {
+    Plugin.fn.foldPanel = function() {
         var
           $i = this.$control.find(".widget-fold").find("i")
         , foldIconClass = ["icon-resize-small", "icon-resize-full"]
@@ -128,7 +189,7 @@
         }
     };
 
-    Plugin.fn.hideEvent = function() {
+    Plugin.fn.hideWidget = function() {
         var that = this;
 
         this.$elem.slideUp(function() {
